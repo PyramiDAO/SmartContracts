6c6f06393d92c1cabbb0547edae4aec0ee1aaedf
CarsonCase <carsonpcase@gmail.com>
2022-02-04 19:51:26 -0500

    tested swaps w/o superfluid

--- contracts/swaps/TestSwapsNoSuperfluid.sol
+++ contracts/swaps/TestSwapsNoSuperfluid.sol
@@ -5,17 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // initializing the CFA Library
-import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
 import "../interfaces/ISwapReceiver.sol";
 
-contract Swaps is ERC721, Ownable{
-
-    using CFAv1Library for CFAv1Library.InitData;
-
+contract TSwaps is ERC721, Ownable{
     uint public index = 0;
-
-    //initialize cfaV1 variable
-    CFAv1Library.InitData public cfaV1;
 
     address public token;
     mapping(uint => uint) public payerRequiredFlowRates;        //Required payments for each payer NFT
@@ -28,17 +21,7 @@ contract Swaps is ERC721, Ownable{
 
     mapping(uint => asset) public receiverAssetsOwed;
 
-    constructor(ISuperfluid host, address _token) Ownable() ERC721("Total Return Swap", "TRS"){
-        //initialize InitData struct, and set equal to cfaV1
-        cfaV1 = CFAv1Library.InitData(
-            host,
-            //here, we are deriving the address of the CFA using the host contract
-            IConstantFlowAgreementV1(
-                address(host.getAgreementClass(
-                        keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
-                    ))
-                )
-            );
+    constructor(address _token) Ownable() ERC721("Total Return Swap", "TRS"){
         token = _token;
     }
 
@@ -46,22 +29,20 @@ contract Swaps is ERC721, Ownable{
 
     /// @dev to be called by strategies. Anyone can make swaps. But it's the strategies that have the assets
     function newSwap(address _receiver, address _payer, uint _requiredFlowRate, uint _amountUnderlying) external{
-        (, int96 initialFlowRate,,) = CFAv1Library.InitData.cfa.getFlow(token, _payer,_receiver);
         require(ISwapReceiver(_receiver).verifyNewSwap(msg.sender,_amountUnderlying), "This receiver did not permit you to issue this swap");
-        require(initialFlowRate >= _requiredFlowRate, "Not paying enough to initialize this swap");
 
-        _mintReceiver(_receiver,_amountUnderlying, msg.sender);         // note receiver will always have an even ID 0,2,4,ect.
-        _mintPayer(_payer,_requiredFlowRate);                           // note payer will always have an odd ID 1,3,5,ect.
+        _mintReceiver(_receiver,_amountUnderlying);         // note receiver will always have an even ID 0,2,4,ect.
+        _mintPayer(_payer,_requiredFlowRate);               // note payer will always have an odd ID 1,3,5,ect.
         emit NewSwap(_receiver, _payer);
     }
 
     // for working on later
     function settleBalances() external{}
 
-    function _mintReceiver(address _receiver, uint _amountUnderlying, address _oracle) internal{
+    function _mintReceiver(address _receiver, uint _amountUnderlying) internal{
         _mint(_receiver,index); 
         // note this will need to use an oracle to get USD value
-        asset memory a =asset(_amountUnderlying, _amountUnderlying, _oracle);
+        asset memory a =asset(_amountUnderlying, _amountUnderlying, address(0));
         _updateReceiverAssetsOwed(index,a);         
         index++;
  
